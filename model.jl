fc45c1a5e90b2eacbeb1b3d6a88582768b52e809
NessInMorse <itsok2001@gmail.com>
2023-07-07 00:53:11 +0200

    Added points table to model

--- model.jl
+++ model.jl
@@ -1,4 +1,5 @@
 
+POINT_TABLE = [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78]
 
 function create_map()::Vector{Vector{Int}}
         #=
@@ -39,7 +40,7 @@ function dice_roll!()::Vector{Int}
         return [rand(1:6) for i in 1:6]
 end
 
-function make_players(n::Int)
+function make_players(n::Int)::Vector{Vector{Int16}}
         #=
         Creates n players with cards
         in:
